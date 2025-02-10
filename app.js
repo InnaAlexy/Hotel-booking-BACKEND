95dc8998296f01edb834c1d2f002538e1a5fe138
InnaAlexy <life.inna.am@yandex.ru>
2025-02-10 18:23:58 +0300

    реализовала register, login, logout

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const { register, login } = require("./controllers/user");
+const mapUser = require("./helpers/mapUser");
 
-const port = 3001;
+const port = 3002;
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
+
+app.post("/register", async (req, res) => {
+  try {
+    const user = await register(req.body.login, req.body.password);
+
+    res.send({ error: null, user: mapUser(user) });
+  } catch (e) {
+    res.send({ error: e.message || "Unckown error" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { user, token } = await login(req.body.login, req.body.password);
+
+    res
+      .cookie("token", token, { httpOnly: true })
+      .send({ error: null, user: mapUser(user) });
+  } catch (e) {
+    res.send({ error: e.message || "Unckown error" });
+  }
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "", { httpOnly: true }).send({});
+});
 
 mongoose
   .connect(
